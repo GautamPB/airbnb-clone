1312cd2c986a908067f11ab22b8b0f20da0fffb5
Gautam <pbg2402@gmail.com>
2021-08-08 02:57:52 +0530

    Added: calendar to search bar

--- components/Header.js
+++ components/Header.js
@@ -3,10 +3,31 @@ import { SearchIcon } from '@heroicons/react/outline'
 import { GlobeAltIcon } from '@heroicons/react/outline'
 import { MenuIcon } from '@heroicons/react/outline'
 import { UserCircleIcon } from '@heroicons/react/solid'
+import { UsersIcon } from '@heroicons/react/solid'
+import { useState } from 'react'
+import { DateRangePicker } from 'react-date-range'
+import 'react-date-range/dist/styles.css'
+import 'react-date-range/dist/theme/default.css'
 
 const Header = () => {
+    const [search, setSearch] = useState('')
+    const [startDate, setStartDate] = useState(new Date())
+    const [endDate, setEndDate] = useState(new Date())
+    const [guests, setGuests] = useState(1)
+
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: 'selection',
+    }
+
+    const handleSelect = (ranges) => {
+        setStartDate(ranges.selection.startDate)
+        setEndDate(ranges.selection.endDate)
+    }
+
     return (
-        <header className="flex md:grid px-2 py-3 md:p-5 bg-white md:grid-cols-3 items-center shadow-lg z-50 sticky top-0">
+        <header className="grid px-2 py-3 md:p-5 bg-white grid-cols-3 items-center shadow-lg z-50 sticky top-0">
             {/* left */}
             <div className="flex h-10 cursor-pointer relative mr-auto">
                 <Image
@@ -23,6 +44,8 @@ const Header = () => {
             {/* middle */}
             <div className="flex items-center border border-gray-200 rounded-[999px] px-4 py-2">
                 <input
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
                     placeholder="Where are you going?"
                     className="w-full bg-transparent outline-none border-none"
                 />
@@ -41,6 +64,31 @@ const Header = () => {
                     <UserCircleIcon className="h-6 text-gray-500 cursor-pointer" />
                 </div>
             </div>
+
+            {search && (
+                <div className="flex flex-col col-span-3 mx-auto">
+                    <DateRangePicker
+                        ranges={[selectionRange]}
+                        minDate={new Date()}
+                        rangeColors={['#fd5861']}
+                        onChange={handleSelect}
+                    />
+
+                    <div className="flex items-center border-b mb-4">
+                        <h2 className="font-bold text-2xl flex-grow">
+                            Number of Guests
+                        </h2>
+                        <UsersIcon className="h-5" />
+                        <input
+                            min={1}
+                            value={guests}
+                            onChange={(e) => setGuests(e.target.value)}
+                            type="number"
+                            className="w-12 pl-2 outline-none text-lg text-red-400"
+                        />
+                    </div>
+                </div>
+            )}
         </header>
     )
 }
